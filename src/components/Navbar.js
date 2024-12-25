5ed616664222bd0b19b4075e3691e5e277cb7423
Pakkapon-Ps <pakkapondz@gmail.com>
2024-12-25 14:21:55 +0700

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,12 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <img src="/img/logo.png" className="d-block" width="115px" alt="Logo" />
+                <img
+                    src={`${process.env.PUBLIC_URL}/img/logo.png`}
+                    className="d-block"
+                    width="115px"
+                    alt="Logo"
+                />
                 <button
                     className="navbar-toggler"
                     type="button"
